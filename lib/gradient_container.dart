856e3044adc75459b5cf1481b035ecef96e715ce
Ash-nep <asharma.it.nepal@gmail.com>
2026-06-10 11:05:41 +0545

    propertises of variables added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -21,7 +21,7 @@ class GradientContainer extends StatelessWidget {
             ),
           ),
           child: Center( 
-            child: StyledText(),
+            child: StyledText('Hello World!'),
               ),
       );      
     }
