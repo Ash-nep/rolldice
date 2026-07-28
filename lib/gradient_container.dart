237839935bbd2db3ed41150f8163e85b3ef01f14
Ash-nep <asharma.it.nepal@gmail.com>
2026-07-29 00:39:58 +0545

    interactive widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:rolldice/styled_text.dart';
+
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-    const GradientContainer(this.color1, this.color2, {super.key});
-  
+    GradientContainer(this.color1, this.color2, {super.key});
+    
+    GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
+  var activeDiceImage = 'assets/images/dice_2.png';
+
+   void rollDice() {
+    activeDiceImage = 'assets/images/dice_4.png';
+   }
+
     @override
     Widget build(context) {
       return Container(
@@ -20,7 +30,26 @@ class GradientContainer extends StatelessWidget {
             ),
           ),
           child: Center( 
-            child: StyledText('Hello World!'),
+            child:Column(
+              mainAxisSize:MainAxisSize.min ,
+              children: [
+              Image.asset(
+                activeDiceImage,
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: rollDice, 
+                style: TextButton.styleFrom(
+                //padding: EdgeInsets.only(
+                  //top: 20,
+                  // ),
+                foregroundColor: Colors.white, 
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                  ),) ,
+                child: const Text('Roll Dice'),)
+            ],), 
               ),
       );      
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:rolldice/gradient_container.dart';
+
 void main(){
    runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 47, 5, 120),
-        body: GradientContainer(const Color.fromARGB(255, 50, 12, 117),
-        const Color.fromARGB(255, 186, 4, 92) ),
+        backgroundColor: const Color.fromRGBO(47, 5, 120, 1),
+        body: GradientContainer (
+          const Color.fromARGB(255, 33, 5, 109),
+          const Color.fromARGB(255, 68, 21, 149),
+        ),
       ), 
     ),
   );
 }
+
 
