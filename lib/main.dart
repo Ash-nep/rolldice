eb29759925def3a64c346b33acddd0526286c6d4
Ash-nep <asharma.it.nepal@gmail.com>
2026-07-29 08:07:20 +0545

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:rolldice/gradient_container.dart';
 
 void main(){
    runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: const Color.fromRGBO(47, 5, 120, 1),
         body: GradientContainer (
-          const Color.fromARGB(255, 33, 5, 109),
-          const Color.fromARGB(255, 68, 21, 149),
+           Color.fromARGB(255, 33, 5, 109),
+            Color.fromARGB(255, 68, 21, 149),
         ),
       ), 
     ),
